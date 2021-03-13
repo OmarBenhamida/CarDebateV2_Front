e17e685c4f4f5828b821d89e5c5b9f29d241066e
OmarBenhamida <omar.benhamida@outlook.com>
2021-03-13 09:29:20 +0100

    commitomar12mars2021

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyGuard } from './Shared/my.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'utilisateur', pathMatch: 'full'},
   //{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule), },
  // tslint:disable-next-line:max-line-length
- { path: 'administrateur', loadChildren: () => import('./administrateur/administrateur.module').then(m => m.AdministrateurModule), /*canActivate: [MyGuard]*/},
+ { path: 'administrateur', loadChildren: () => import('./administrateur/administrateur.module').then(m => m.AdministrateurModule), canActivate: [MyGuard]},
  // tslint:disable-next-line:max-line-length
  { path: 'utilisateur', loadChildren: () => import('./utilisateur/utilisateur.module').then(m => m.UtilisateurModule), /*canActivate: [MyGuard]*/},
  { path: 'authentification', loadChildren: () => import('./authentification/authentification.module').then(m => m.AuthentificationModule), /*canActivate: [MyGuard]*/},
